2775ac55456e264f3909b322ec6d972f98899794
HuyBunn <130323865+HuyBunn@users.noreply.github.com>
2024-01-20 09:53:56 +0700

    v1.1.1

---
+++ 03-functions-scope/09-function-challenges/08-function-challenges.md/script.js
@@ -0,0 +1,21 @@
+const getCelsius = (f) => f;
+console.log(`The temperature is ${getCelsius(32)} \xB0C`);
+
+function minMax(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  return {
+    min,
+    max,
+  };
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
+((length, width) => {
+  const area = length * width;
+
+  const output = `The area of a rectangle with a length of ${length} and a width of ${width} is ${area}.`;
+
+  console.log(output);
+})(50, 10);
